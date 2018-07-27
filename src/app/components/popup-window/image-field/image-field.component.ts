45df703eade9acddc0553dff4b6d708438e280de
Alemina <kobylakk@wp.pl>
2018-07-27 16:12:47 +0200

    display uploaded image in img tag

--- src/app/components/popup-window/image-field/image-field.component.ts
+++ src/app/components/popup-window/image-field/image-field.component.ts
@@ -1,23 +1,37 @@
-import { Component } from '@angular/core';
-import { Message } from '../../../data/message';
+import { Component, OnInit } from '@angular/core';
+import { Message } from '../../../data/message.model';
 
 @Component({
   selector: 'app-image-field',
   templateUrl: './image-field.component.html',
   styleUrls: ['./image-field.component.css']
 })
-export class ImageFieldComponent {
+export class ImageFieldComponent implements OnInit {
 
   msgs: Message[];
 
   uploadedFiles: any[] = [];
 
-  onUpload(event) {
+  url: any;
+  isDone: boolean;
+
+  test: string;
+
+  ngOnInit(){
+    this.isDone = false;
+  }
+
+  myUploader(event) {
+      console.log('here');
       for (const file of event.files) {
+          console.log('asd ', file);
           this.uploadedFiles.push(file);
+          this.url = file.objectURL;
+          this.isDone = true;
       }
       this.msgs = [];
       this.msgs.push({severity: 'info', summary: 'File Uploaded', detail: ''});
+      this.test = `${this.test} asd `;
   }
 
 }
