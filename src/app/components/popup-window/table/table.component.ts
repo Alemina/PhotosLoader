5e8b42fe1e2bba42fe4010df9abc9eb6d1e944a8
Alemina <kobylakk@wp.pl>
2018-07-29 10:50:32 +0200

    - fixed pagination - added search and filter

--- src/app/components/popup-window/table/table.component.ts
+++ src/app/components/popup-window/table/table.component.ts
@@ -9,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class TableComponent implements OnInit {
 
   table: any[];
+  cols: any[];
+
 
   ngOnInit() {
     this.table = [
@@ -16,12 +18,22 @@ export class TableComponent implements OnInit {
       { image: '/assets/images/example_image_2.png', description: '83%'},
       { image: '/assets/images/example_image_3.png', description: '38%'}
     ];
+    this.cols = [
+      { field: 'image', header: 'Image' },
+      { field: 'description', header: 'Description' }
+    ];
   }
   myUploader(event){
     console.log('myUploader ', event)
+    console.log('myUploader ', event.files)
+    console.log('myUploader ', event.files[0].objectURL.changingThisBreaksApplicationSecurity)
+    // this.table.push({image: event.files[0].objectURL.changingThisBreaksApplicationSecurity})
+    this.table.push({ image: '/assets/images/example_image_3.png', description: '38%'})
+    console.log(this.table);
   }
   myAdd(){
     console.log('add')
+    this.table = [];
   }
   onRowSelect(event){
     console.log('onRowSelect ', event)
